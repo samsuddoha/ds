0d1a5b23b0a71bbd84d5178d460453ff70f5d791
MSD <mdsamsuddoha@192.168.1.2>
2026-05-08 23:15:54 +0600

    bs

---
+++ Code/Searching/bs.cpp
@@ -0,0 +1,46 @@
+#include<stdio.h>
+
+int binarySearch(int arr[], int N, int SK){
+    int LI=0;
+    int RI = N-1;
+    int MI;
+    while(LI<=RI){
+        MI=(LI+RI)/2;
+        if(SK==arr[MI]) 
+            return MI;
+        else if(SK>arr[MI])
+            LI=MI+1;
+        else
+            RI=MI-1;
+    }
+    return -1;
+}
+
+int main()
+{
+    int N;
+    //printf("Enter the size of array.");
+    scanf("%d", &N);
+    int A[N];
+    for(int i=0; i<N;i++){
+        scanf("%d", &A[i]);
+    }
+    int searchKey;
+    scanf("%d", &searchKey);
+    int isFound=binarySearch(A, N, searchKey);
+    if(isFound<0) 
+        printf("Not found");
+    else 
+        printf("%d is found in %d\n", searchKey,isFound);
+    return 0;
+}
+
+/*
+5
+10 23 34 56 76
+56
+
+10
+12 13 14
+
+*/
